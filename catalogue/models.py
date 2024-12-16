15bccf40cbd29887b1b8546406a9da8c547ad051
AbdulHaris633 <haris611@outlook.com>
2024-12-16 19:21:53 +0500

    updated product model

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.core.validators import MinValueValidator
 
 class ProductClass(models.Model):
     id= models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
@@ -21,11 +22,13 @@ class Category(models.Model):
 class Product(models.Model):
     category=models.ForeignKey(Category,on_delete=models.CASCADE)
     id= models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
-    name=models.CharField(max_length=50,null=True)
-    description=models.TextField(max_length=255,blank=True)
-    image=models.ImageField(upload_to="images/",blank=True)
+    name=models.CharField(max_length=50,null=True)   
+    rating=models.DecimalField(max_digits=3,decimal_places=2,default=0.00,null=True,validators=[MinValueValidator(0)])
+    price=models.FloatField(null=True,blank=True,default=0.0) 
+    description=models.TextField(max_length=255,blank=True,null=True)
+    image=models.ImageField(upload_to="images/",blank=True,null=True)
     
     def __str__(self):
-        return self.name  
+        return self.name   
     
     
