5d0eeabf9c191d47f2764cd6a74c0127af476516
AbdulHaris633 <haris611@outlook.com>
2025-03-23 06:16:49 +0500

    refacotr

--- src/settings.py
+++ src/settings.py
@@ -125,9 +125,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
+    ), 
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
-}
+} 
 
 IMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(
